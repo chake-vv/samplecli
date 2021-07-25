cba9394bf70c8a84a41fa405270608e059ec0626
chake-vv <chake-vv@gmail.com>
2021-07-26 00:23:43 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN programs",
+    version = "1.0.0",
+    author = "Your names",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
+}
